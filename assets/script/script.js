896715f649f4b0e411689ef518446dbd5ba6f237
Elle <bound_by_harmony@yahoo.com>
2021-02-12 16:00:53 +0000

    Portfolio Update, click functions added

---
+++ assets/script/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $("#ab-me-btn").on("click", function(){
+      $("#l-pg").addClass("d-none");
+      $("#projects-pg").addClass("d-none");
+      $("#contact-pg").addClass("d-none");
+      $("#about-me").removeClass("d-none");
+    });
+    $("#projects-btn").on("click", function(){
+      $("#l-pg").addClass("d-none");
+      $("#about-me").addClass("d-none");
+      $("#contact-pg").addClass("d-none");
+      $("#projects-pg").removeClass("d-none");
+    });
+    $("#contact-btn").on("click", function(){
+      $("#l-pg").addClass("d-none");
+      $("#about-me").addClass("d-none");
+      $("#projects-pg").addClass("d-none");
+      $("#contact-pg").removeClass("d-none");
+    });
+    $("#home-pg-btn").on("click", function(){
+      $("#about-me").addClass("d-none");
+      $("#projects-pg").addClass("d-none");
+      $("#contact-pg").addClass("d-none");
+      $("#l-pg").removeClass("d-none");
+    });
+  });
